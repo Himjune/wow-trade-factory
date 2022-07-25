56c6067c8a9692461f006f0de8f1618bd969f4be
Himjune <himjune@mail.ru>
2022-07-25 03:39:21 +0300

    onLoad event

--- main.lua
+++ main.lua
@@ -1,6 +1,18 @@
-
+wtfacAucDump = {}  -- default value until ADDON_LOADED
 
 SLASH_WTFAC1 = "/wtfac"
 SlashCmdList["WTFAC"] = function(msg)
-    message('My first addon!'..msg)
+    message('My first addon!'..msg);
 end
+
+local frame = CreateFrame("FRAME", "FooAddonFrame");
+frame:RegisterEvent("ADDON_LOADED");
+local function eventHandler(event, ...)
+    local event, arg1, arg2, arg3, arg4 = select(1,...);
+    print("WTFac event "..event.." - "..arg1);
+
+    if event == "ADDON_LOADED" and arg1 == "WowTradeFactory" then
+        print("WTFac loaded");
+    end
+end
+frame:SetScript("OnEvent", eventHandler);
