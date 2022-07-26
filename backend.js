bb607aeee9a122d34815bd9111faab067ca9066f
Himjune <himjune@mail.ru>
2022-07-26 10:22:44 +0300

    lua file line parse

--- backend.js
+++ backend.js
@@ -10,31 +10,44 @@ const readline = require('readline');
 const wow_path = "C:\\Program Files (x86)\\World of Warcraft\\_classic_";
 const horde_acc = "101624645#1";
 
+function parseLuaFileLine(line) {
+  line = line.replace(/[\t,\"\[\]]+/gm, "").trim();
+  console.log("|"+line+"|");
+  let kv = line.split(" = ");
+  return {key: kv[0], val: kv[1]};
+}
+
 const parseSavedVarsFile = async (path) => {
   const fileStream = fs.createReadStream(path);
-  const arr = [];
 
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
   });
 
-  const it = rl[Symbol.asyncIterator]();
-  const line1 = await it.next();
-  
-  arr.push(await (await it.next()).value);
-  await it.next();
-  await it.next();
-  arr.push(await (await it.next()).value);
-  arr.push(await (await it.next()).value);
+  const parsedVars = {};
 
-  return arr;
+  const it = rl[Symbol.asyncIterator]();
+  //const line1 = await it.next();
+  let lineObj;
+  do {
+    lineObj = await it.next();
+    if (lineObj.done) continue;
+
+    let parsedLine = parseLuaFileLine(lineObj.value);
+    parsedVars[parsedLine.key] = {};
+    parsedVars[1.43] = "eve";
+    parsedVars[4] = "ava";
+  } while (lineObj.done == false);
+
+  console.log("R", parsedVars);
+  return parsedVars;
 }
 
 app.get('/', (req, res) => {
   (async () => {
-    let arr = await parseSavedVarsFile(wow_path + "\\WTF\\Account\\" + horde_acc + "\\SavedVariables\\WowTradeFactory.lua", res);
-    res.send(JSON.stringify(arr));
+    let parsedVars = await parseSavedVarsFile(wow_path + "\\WTF\\Account\\" + horde_acc + "\\SavedVariables\\WowTradeFactory.lua");
+    res.send(JSON.stringify(parsedVars));
   })();
 })
 
