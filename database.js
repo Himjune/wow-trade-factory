477f5594953740ac760d8c52c9dc6cfa1bbc38a9
Himjune <himjune@mail.ru>
2022-08-01 05:41:11 +0300

    db module refactor: no pre-creation for collections

--- database.js
+++ database.js
@@ -8,17 +8,6 @@ exports.get_dbo = new Promise((resolve, reject) => {
         if (err) reject(err);  
         dbo = db.db("wtfacdb");
         console.log("wtfacdb Database created(connected)!");
-        
-        dbo.createCollection("stats", function(err, res) {
-            if (err) console.log("stats Collection err! "+err);
-            else console.log("stats Collection created!");
-        });
-
-        dbo.createCollection("mails", function(err, res) {
-            if (err) console.log("mails Collection err! "+err);
-            else console.log("mails Collection created!");
-        });
-
         resolve(dbo);
     });
 }); 
